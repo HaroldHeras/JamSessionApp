d21c994f0797b65ec56969237784b65a8fb56763
Harold de las Heras <haroldherascabezas@hotmail.com>
2025-07-08 20:15:09 +0200

    Servidor iniciado

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+import express from "express";
+
+const app = express();
+
+
+const port = 8080;
+
+
+app.use(express.static("public"));
+
+
+
+app.listen(port, ()=>{
+    console.log(`Todo listo en el puerto ${port}`);
+});
