ce4e2e768eddad16c755f0c06bcc594595be362c
Harold de las Heras <haroldherascabezas@hotmail.com>
2025-07-22 22:14:40 +0200

    Experimentos con Angular

---
+++ client/public/front/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { LogIn } from './pages/log-in/log-in';
+
+export const routes: Routes = [
+    {path: "LogIn", component: LogIn}
+];
---
+++ client/public/front/src/app/pages/log-in/log-in.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-log-in',
+  imports: [RouterModule],
+  templateUrl: './log-in.html',
+  styleUrl: './log-in.css'
+})
+export class LogIn {
+
+}
